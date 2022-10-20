553f53bab360fc7b361b3448324de71f330b5660
Viktar <victorenator@gmail.com>
2022-10-20 13:31:50 +0300

    use node:test

--- test.js
+++ test.js
@@ -1,18 +1,17 @@
 import {strictEqual} from 'node:assert';
+import test from 'node:test';
 import {transformFileAsync} from '@babel/core';
 
-describe('transform', () => {
-    it('returns relative imports', async () => {
-        const result = await transformFileAsync(new URL('test/abc.js', import.meta.url).pathname, {
-            babelrc: false,
-            plugins: [
-                './index.js'
-            ]
-        });
-        strictEqual(result.code, `\
+test('transform returns relative imports', async () => {
+    const result = await transformFileAsync(new URL('test/abc.js', import.meta.url).pathname, {
+        babelrc: false,
+        plugins: [
+            './index.js'
+        ]
+    });
+    strictEqual(result.code, `\
 import { LitElement } from "./node_modules/lit/index.js";
 import { transform } from "../node_modules/@babel/core/lib/index.js";
 transform();
 export class New extends LitElement {}`);
-    });
 });
